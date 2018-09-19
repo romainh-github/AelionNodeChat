60e0b82297bcf624438d16988cc78aaefbd1e43b
romainh <romainh.fr@gmail.com>
2018-09-19 17:08:40 +0200

    wip connection to websocket server

--- chat/src/app/app.component.ts
+++ chat/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebSocketSubject } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chat';
+  title = 'Websocket chat';
+
+  /**
+   * Instance of WebSocketSubject
+   */
+  private _socket: WebSocketSubject<any>;
+
+  /**
+   * @var :store messages
+   */
+  public serverMessages: String[];
+
+  constructor () {
+    console.log('Connection to client WebSocket');
+    this._socket = new WebSocketSubject('ws://127.0.0.1:8999');
+    // test connection to outside
+    this._send();
+    // subscribe to server events
+    this._socket.subscribe((message) => {
+      console.log('Server sent :' + message);
+      this.serverMessages.push(message);
+      },
+      (err) => console.error(err),
+      () => console.warn('Completed !')
+    );
+  }
+
+  private _send(): void {
+    console.log('Send new message to server');
+    this._socket.next('New client connected');
+  }
 }
